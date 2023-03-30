fd7181bc3591a0f34c99513d625c8a08a93daa0a
wwsheng009 <vincentwwsheng@gmail.com>
2023-03-31 00:46:43 +0800

    更新处理脚本

--- cleanup.sh
+++ cleanup.sh
@@ -1,17 +1,33 @@
 
 kill -9 $(ps aux | grep 'yao' | awk '{print $2}')
 
-rm -rf charts/*
-rm -rf flows/app/*
 rm -rf logins/*
 rm -rf logs/*
-rm -rf models/*
-rm -rf tables/*
-rm -rf forms/*
+
+
+
+find models/ -not -path "models/" -not -path "models/sys*"   | xargs rm -rf
+find tables/ -not -path "tables/" -not -path "tables/sys*" | xargs rm -rf
+find forms/ -not -path "forms/" -not -path "forms/sys*" | xargs rm -rf
+find flows/ -not -path "flows/" -not -path "flows/sys*" | xargs rm -rf
+find charts/ -not -path "charts/" -not -path "charts/sys*" | xargs rm -rf
+
 # rm -rf .env
 rm -rf .trash/*
 rm -rf db/*
 
+mkdir -p tables/app
+mkdir -p tables/sys
+
+mkdir -p forms/app
+mkdir -p forms/sys
+
+mkdir -p flows/app
+mkdir -p flows/sys
+mkdir -p models/app
+mkdir -p models/sys
+mkdir -p charts/app
+mkdir -p charts/sys
 
 # rm -rf ./app.json
 
